cd859a4ce64dc106e27b9d410fa834a86433de23
Paul Hutchinson <hutchinson.paul@gmail.com>
2025-04-26 18:44:38 -0400

    fixed up release notes

--- src/ReleaseNotes.c
+++ src/ReleaseNotes.c
@@ -20,23 +20,7 @@ struct ReleaseInfo
 //    {   "Title",
 //            "Descrition"},
 //};
-static struct ReleaseNote m_Notes_Rel1_0_2_0[]=
-{
-    {   "Fixed problem with Linux installer",
-            "The Linux installer was missing a depends for the QT multimedia.  Added it and added version numbers to the packages."},
-    {   "Fixed bug with open new connection from URI dialog",
-            "Dialog was not setting up options which depending on the device would prevent connection from working."},
-    {   "Fixed bug with importing settings does not apply the settings",
-            "When you imported new settings the settings where not being "
-            "applied to connections, but new connections would get the "
-            "settings."},
-};
-static struct ReleaseNote m_Notes_Rel1_0_1_0[]=
-{
-    {   "Fixed bug with plugins with long descriptions",
-            "The wrong buffer was used when loading a plugin with a description longer than 99 bytes"},
-};
-static struct ReleaseNote m_Notes_Rel1_0_0_0[]=
+static struct ReleaseNote m_Notes_NEXT[]=
 {
     {   "Added a help button to URI input",
             "A new help button was added after the URI input on the main window.  "
@@ -50,8 +34,16 @@ static struct ReleaseNote m_Notes_Rel1_0_0_0[]=
             "Under the Settings menu a new menu item was added that lets "
             "the user set settings back to defaults."},
 };
+static struct ReleaseNote m_Notes_Rel1_0_3_0[]=
+{
+    {   "Fixed problem where file download didn't know what xmodem was selected by default",
+            "When you went to select download a file you needed to click on the"
+            " protocol input or it would just keep giving an error."},
+};
 static struct ReleaseNote m_Notes_Rel1_0_2_0[]=
 {
+    {   "Fixed problem with Linux installer",
+            "The Linux installer was missing a depends for the QT multimedia.  Added it and added version numbers to the packages."},
     {   "Fixed bug with open new connection from URI dialog",
             "Dialog was not setting up options which depending on the device would prevent connection from working."},
     {   "Fixed bug with importing settings does not apply the settings",
@@ -73,6 +65,7 @@ static struct ReleaseNote m_Notes_Rel1_0_0_0[]=
 const struct ReleaseInfo m_ReleaseNotes[]=
 {
 //    {"x.x.x.x","dd Mmm yyyy","NEXT",m_Notes_NEXT,sizeof(m_Notes_NEXT)/sizeof(struct ReleaseNote)},
+//    {"1.0.3.0","xx Apr 2025",NULL,m_Notes_Rel1_0_3_0,sizeof(m_Notes_Rel1_0_3_0)/sizeof(struct ReleaseNote)},
     {"1.0.2.0","23 Apr 2025",NULL,m_Notes_Rel1_0_2_0,sizeof(m_Notes_Rel1_0_2_0)/sizeof(struct ReleaseNote)},
     {"1.0.1.0","16 Mar 2025",NULL,m_Notes_Rel1_0_1_0,sizeof(m_Notes_Rel1_0_1_0)/sizeof(struct ReleaseNote)},
     {"1.0.0.0","01 Mar 2025","First Release",m_Notes_Rel1_0_0_0,sizeof(m_Notes_Rel1_0_0_0)/sizeof(struct ReleaseNote)},

91aa884031962c094df22a179d634a0a2b34e81b
Paul Hutchinson <hutchinson.paul@gmail.com>
2025-10-03 20:37:03 -0400

    Updated release notes for the 2.0.2 version

--- src/ReleaseNotes.c
+++ src/ReleaseNotes.c
@@ -28,7 +28,7 @@ struct ReleaseInfo
 //            "Descrition"},
 //};
 
-static struct ReleaseNote m_Notes_NEXT[]=
+static struct ReleaseNote m_Notes_Rel2_0_2_0[]=
 {
     {   "Fixed bug with the com test crashing when used with serial ports",
             "The serial port driver was setting aux controls even if they"
@@ -330,6 +330,7 @@ static struct ReleaseNote m_Notes_Rel1_0_0_0[]=
 const struct ReleaseInfo m_ReleaseNotes[]=
 {
 //    {RELEASE_NOTE("x.x.x.x","-- -- ----",NULL,m_Notes_NEXT)},
+    {RELEASE_NOTE("2.0.2.0","03 Oct 2025",NULL,m_Notes_Rel2_0_2_0)},
     {RELEASE_NOTE("2.0.1.0","30 Sep 2025",NULL,m_Notes_Rel2_0_1_0)},
     {RELEASE_NOTE("2.0.0.0","26 Sep 2025","Updates to the plugin system",m_Notes_Rel2_0_0_0)},
     {RELEASE_NOTE("1.1.0.0","29 Jun 2025","Added most requested features",m_Notes_Rel1_1_0_0)},
